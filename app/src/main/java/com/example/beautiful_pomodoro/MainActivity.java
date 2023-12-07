2e8036942e385bad970a30b8bce8280fb7d3c710
Luis N <luinungom@gmail.com>
2023-12-07 21:33:20 +0100

    First commit!

---
+++ app/src/main/java/com/example/beautiful_pomodoro/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.beautiful_pomodoro;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+
+import com.example.beautiful_pomodoro.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ConstraintLayout constraintLayout = findViewById(R.id.layout);
+
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(5000);
+        animationDrawable.start();
+    }
+
+}
